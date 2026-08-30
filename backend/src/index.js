781ecfd9b12d3f5caed8c70daf0b593b7656c3f1
Vinay Sachdeva <vinaysachdeva407@gmail.com>
2026-08-31 02:19:19 +0530

    updated

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,17 +1,18 @@
+
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import cors from "cors";
 import path from "path";
-import { connectDB } from "./lib/db.js";
 
+import { connectDB } from "./lib/db.js";
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
 import { app, server } from "./lib/socket.js";
 
 dotenv.config();
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 5000;
 const __dirname = path.resolve();
 
 app.use(express.json());
@@ -19,9 +20,7 @@ app.use(cookieParser());
 
 app.use(
   cors({
-    origin: [
-      "http://localhost:5173",
-    ],
+    origin: ["http://localhost:5173"],
     credentials: true,
   })
 );
@@ -29,16 +28,21 @@ app.use(
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
-if(process.env.NODE_ENV === "production"){
-  app.use(express.static(path.join(_dirname, "../frontend/dist")));
+// Serve frontend in production
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
-})
+  app.get("*", (req, res) => {
+    res.sendFile(
+      path.join(__dirname, "../frontend/dist/index.html")
+    );
+  });
 }
+
+// Connect to MongoDB
 connectDB();
+
+// Start server
 server.listen(PORT, () => {
-  console.log("server is running on PORT:" + PORT);
-  
+  console.log(`Server is running on PORT: ${PORT}`);
 });
