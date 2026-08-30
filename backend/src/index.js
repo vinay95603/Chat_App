f09e3981a762b4412af08191943cbf9436a785f4
Vinay Sachdeva <vinaysachdeva407@gmail.com>
2026-08-31 01:59:34 +0530

    updated

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,7 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import cors from "cors";
-
+import path from "path";
 import { connectDB } from "./lib/db.js";
 
 import authRoutes from "./routes/auth.route.js";
@@ -12,6 +12,7 @@ import { app, server } from "./lib/socket.js";
 dotenv.config();
 
 const PORT = process.env.PORT;
+const __dirname = path.resolve();
 
 app.use(express.json());
 app.use(cookieParser());
@@ -20,7 +21,6 @@ app.use(
   cors({
     origin: [
       "http://localhost:5173",
-      "https://chat-app-w8xo.onrender.com",
     ],
     credentials: true,
   })
@@ -28,6 +28,15 @@ app.use(
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
+
+if(process.env.NODE_ENV === "production"){
+  app.use(express.static(path.join(_dirname, "../frontend/dist")));
+
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+})
+}
 
 server.listen(PORT, () => {
   console.log("server is running on PORT:" + PORT);
